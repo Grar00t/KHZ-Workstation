ddfc070b34d57e53aa651d3146b99e259a278994
SULIMAN ALSHAMMARI <239743497+Grar00t@users.noreply.github.com>
2026-09-10 00:41:18 +0300

    fix(reporting): disambiguate WPF Application type

--- experiments/KHZ.Reporting.Spike/App.xaml.cs
+++ experiments/KHZ.Reporting.Spike/App.xaml.cs
@@ -3,7 +3,7 @@ using Majorsilence.Reporting.Rdl;
 
 namespace KHZ.Reporting.Spike;
 
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
