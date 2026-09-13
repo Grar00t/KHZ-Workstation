a46d6dceb7d04c3cca3f22800f654a7276feb215
SULIMAN ALSHAMMARI <239743497+Grar00t@users.noreply.github.com>
2026-09-13 11:04:05 +0300

    fix(asset-register): import System.IO for CSV and database checks

--- windows/KHZ.AssetRegister/MainWindow.xaml.cs
+++ windows/KHZ.AssetRegister/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Data;
